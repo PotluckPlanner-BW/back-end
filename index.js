54507d40979800a5421e73ff15a612b7c3084bd8
Valerie <53797814+valerievazq@users.noreply.github.com>
2021-02-04 16:58:04 -0700

    database test 3

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
+
 server.get("/", async (req, res) => {
   try {
-    const shoutouts = await db("shoutouts");
-    const messageOfTheDay = process.env.MOTD || "Hello World!"; // add this line
-    res.status(200).json({ motd: messageOfTheDay, shoutouts }); // change this line
+    const messageOfTheDay = (await process.env.MOTD) || "Hello World!"; // add this line
+    res.status(200).json({ motd: messageOfTheDay }); // change this line
   } catch (error) {
     console.error("\nERROR", error);
     res.status(500).json({ error: "Cannot retrieve the shoutouts" });
